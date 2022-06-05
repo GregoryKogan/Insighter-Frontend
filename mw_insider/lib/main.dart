f8955402267d15430f61c4a1d39484ba8000ed58
GregoryKogan <grisha.koganovskiy@gmail.com>
2022-06-05 21:59:58 +0300

    Loading page created

--- mw_insider/lib/main.dart
+++ mw_insider/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:mw_insider/pages/loading.dart';
 import 'package:mw_insider/pages/main_page.dart';
 import 'package:mw_insider/pages/unknown_route.dart';
 import 'package:mw_insider/theming/theme_service.dart';
@@ -19,18 +20,26 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'MW Insider',
-      initialRoute: '/',
+      initialRoute: '/loading',
       theme: Themes.light,
       darkTheme: Themes.dark,
       themeMode: ThemeService().theme,
       unknownRoute:
           GetPage(name: '/not_found', page: () => const UnknownRoutePage()),
       getPages: [
+        GetPage(name: '/', page: () => MainPage(), transition: Transition.fade),
         GetPage(
-            name: '/',
-            page: () => MainPage(),
-            transition: Transition.noTransition),
+            name: '/loading',
+            page: () => const LoadingPage(),
+            transition: Transition.fade),
       ],
     );
   }
 }
+
+
+// TODO: Loading page checks if refresh token is still ~fresh~
+// TODO: If not, it opens login/sign up page
+// TODO: JWT tokens should be stored in secure_storage
+// TODO: JWT tokens should be appended to every request
+// TODO: Access token should be automatically updated 
---
+++ mw_insider/lib/pages/loading.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mw_insider/configuration/config.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+Future<String> pingServer() async {
+  final response = await http.get(Uri.parse('${backendUrl}ping'));
+
+  if (response.statusCode == 200) {
+    return response.body;
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+Future<void> validateResponse(String response) async {
+  if (response != '') Get.offNamed('/');
+}
+
+class LoadingPage extends StatefulWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingPage> createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  late Future<String> serverResponse;
+
+  @override
+  void initState() {
+    super.initState();
+    serverResponse = pingServer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: FutureBuilder<String>(
+        future: serverResponse,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) validateResponse(snapshot.data!);
+          return const CircularProgressIndicator();
+        },
+      )),
+    );
+  }
+}
